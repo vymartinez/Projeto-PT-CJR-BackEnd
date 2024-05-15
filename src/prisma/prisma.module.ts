5d4735b67ffe569b55c120f7640918b5d91afbcf
aadriellyy <limaadrielly214@gmail.com>
2024-05-14 22:56:59 -0300

    adicionando o validator

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,11 @@
 import {PrismaService} from "./prisma.service";
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 
 /*colocamos aqui o que vamos importar e exportar (nesse caso não vamos importar nada) */
-@Global() /*esse é um decorater, pega um método e atribui algum comportamento a ele (o comportamento atribuido fica dentro c=dos colchetes)*/
+/*esse é um decorater, pega um método e atribui algum comportamento a ele (o comportamento atribuido fica dentro c=dos colchetes)*/
+@Global()
 @Module({
     providers: [PrismaService],
-    exports: [PrismaService],
-});
+    exports: [PrismaService]
+})
+export class PrismaModule {};
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body(ValidationPipe) userData: CreateUserDto){
+    return userData;
+  }
 }
+
