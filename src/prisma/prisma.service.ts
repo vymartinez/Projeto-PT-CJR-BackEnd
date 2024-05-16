db70b9c41bf5db789b97594ddf605ebc782268a9
aadriellyy <limaadrielly214@gmail.com>
2024-05-16 00:00:39 -0300

    adicionando configurações do banco de dados

--- src/prisma/prisma.service.ts
+++
@@ -1,7 +0,0 @@
-/*prisma client = é oq de fato nos permite trabalhar com o ROM*/
-import { PrismaClient } from "@prisma/client";
-import { Injectable } from "@nestjs/common"; /*trabalha com injeção de dependencias (um serviço é injetado dentro de outro) */
-
-@Injectable()
-/*o prismaService vai fazer o papel do prismaClient no nosso programa */
-export class PrismaService extends PrismaClient {};
