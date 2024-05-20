6c8d2a964025256870319b5b423d3bfed15f1bc6
sarahnovaiss <sarahnm.novais@gmail.com>
2024-05-20 01:29:05 -0300

    CRUD do usuário

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,43 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+}
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async create(@Body(ValidationPipe) userData: CreateUserDto){
-    return userData;
+  createUser(@Body(ValidationPipe) userData: CreateUserDto): User {
+    return this.userService.createUser(userData);
+  }
+
+
+  @Get()
+  findAll(): User[] {
+    return this.userService.findAll();
+  }
+  
+
+  @Get(':id')
+  findUser(@Param('id') id: number): User {
+    return this.userService.findUser(Number(id));
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: number, @Body(ValidationPipe) updateData: UpdateUserDto): User {
+    return this.userService.updateUser(Number(id), updateData);
+  }
+  
+  @Delete(':id')
+  deleteUser(@Param('id') id: number): void {
+    return this.userService.deleteUser(Number(id));
   }
 }
-
