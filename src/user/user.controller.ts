b62f6ad94a53817b15b7e50b84465771684f8a86
Victor Martinez <victoryanmartinez@gmail.com>
2024-06-02 18:52:27 -0300

    changing user CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,29 +12,22 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  password: string;
-}
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  createUser(@Body(ValidationPipe) userData: CreateUserDto): User {
+  createUser(@Body(ValidationPipe) userData: CreateUserDto) {
     return this.userService.createUser(userData);
   }
 
   @Get()
-  findAll(): User[] {
+  findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
-  findUser(@Param('id') id: number): User {
+  findUser(@Param('id') id: number) {
     return this.userService.findUser(Number(id));
   }
 
@@ -42,12 +35,12 @@ export class UserController {
   updateUser(
     @Param('id') id: number,
     @Body(ValidationPipe) updateData: UpdateUserDto,
-  ): User {
+  ) {
     return this.userService.updateUser(Number(id), updateData);
   }
 
   @Delete(':id')
-  deleteUser(@Param('id') id: number): void {
+  deleteUser(@Param('id') id: number) {
     return this.userService.deleteUser(Number(id));
   }
 }
