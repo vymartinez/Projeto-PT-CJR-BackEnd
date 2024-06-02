498b8ec64d380388fd91e82a80e78c380aaff620
Victor Martinez <victoryanmartinez@gmail.com>
2024-06-02 20:15:02 -0300

    reverting commit

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-  await app.listen(3001);
+  await app.listen(3000);
 }
-
-
 bootstrap();
 
 //ate agora apenas iniciamos nosso ouvinte HTTP, que permite que o aplicativo aguarde solicitações HTTP de entrada.
--- src/teacher/teacher.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { teacherService } from './teacher.service';
-import { TeacherController } from './teacher.controller';
-
-@Module({
-  controllers: [TeacherController],
-  providers: [teacherService],
-})
-export class TeacherModule {}
